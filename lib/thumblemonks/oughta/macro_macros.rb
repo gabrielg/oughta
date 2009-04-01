df714a5b6deb22daacebcaa5a13fe22350e41e3a
Gabriel Gironda <gabriel.gironda@gmail.com>
2009-04-01 17:07:31 -0500

    Version bump to 0.1.1

--- lib/thumblemonks/oughta/macro_macros.rb
+++ lib/thumblemonks/oughta/macro_macros.rb
@@ -3,12 +3,46 @@ module Thumblemonks
     module MacroMacros
       module ClassMethods
         
+        # Defines a macro for use in a TestCase or Context with the given name and block.
+        # For example, this defines a macro called :should_be_cool
+        #
+        #   macro :should_be_cool do
+        #     
+        #     should("be true") { assert(true) }
+        #     should("be cool") { assert_equal("cool", @message) }
+        #     should_eventually "do something else"
+        #
+        #   end
+        #
+        # To then use this macro, see the documentation for use_macro
+        #
         def macro(name, &block)
           (class << self; self; end).instance_eval do 
             define_method(:"Macro: #{name}") { |context| context.instance_eval(&block) }
           end
         end
         
+        # Uses a macro defined using the macro method. For example:
+        #
+        #   class TooLazyToSubmitPatchesToThoughtbotTest < Test::Unit::TestCase 
+        #
+        #     macro :should_be_cool do
+        #       should("be cool") { assert_equal("cool", @message) }
+        #     end
+        #
+        #     def setup
+        #       @message = "cool"
+        #     end
+        #     
+        #     use_macro :should_be_cool
+        #
+        #     context "intentionally fails" do
+        #       setup { @message = "not cool yo" }
+        #       use_macro :should_be_cool
+        #     end
+        #
+        #   end
+        #
         def use_macro(name)
           send(:"Macro: #{name}", Shoulda.current_context || self)
         end
